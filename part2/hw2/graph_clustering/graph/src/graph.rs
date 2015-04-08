71665b52abd96abe01556e41309f4edaf1a3bf12
David Blinn <dblinn@cyrusinnovation.com>
2015-04-07 21:18:03 -0400

    add additional crate for graph

---
+++ part2/hw2/graph_clustering/graph/src/graph.rs
@@ -0,0 +1,4 @@
+#[derive(Debug)]
+pub struct Graph {
+	pub n: i32,
+}
--- part2/hw2/graph_clustering/src/main.rs
+++ part2/hw2/graph_clustering/src/main.rs
@@ -1,3 +1,14 @@
+#![feature(core)]
+
+use std::error::Error;
+use std::io::BufReader;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use graph::graph::{Graph};
+
+extern crate graph;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {:?}", Graph {n: 32});
 }
