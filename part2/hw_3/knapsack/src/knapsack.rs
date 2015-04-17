16455a42f2071a418b5959d16c98d11363e97ecf
David Blinn <dblinn@cyrusinnovation.com>
2015-04-17 18:52:19 -0400

    more progress on knapsack

--- part2/hw_3/knapsack/src/knapsack.rs
+++ part2/hw_3/knapsack/src/knapsack.rs
@@ -9,14 +9,14 @@ impl Item {
 	}
 }
 
-#[derive(Copy)]
+#[derive(Clone, Copy)]
 pub struct Memo {
 	pub optimal_value: u32,
 	pub used_weight: u32,
 }
 
 impl Memo {
-	pub fn Add(&self, item: &Item) -> Memo {
+	pub fn add(&self, item: &Item) -> Memo {
 		Memo { optimal_value: self.optimal_value + item.value, used_weight: self.used_weight + item.weight }
 	}
 
@@ -32,38 +32,55 @@ impl Memo {
 pub struct Solver {
 	pub items: Vec<Item>,
 	pub knapsack_size: u32,
-	memo: Vec<Memo>,
+	first_memo: Vec<Memo>,
+	second_memo: Vec<Memo>,
 }
 
 impl Solver {
 	pub fn new(items: Vec<Item>, knapsack_size: u32) -> Solver {
-		let mut memo = Solver::create_memo(knapsack_size);
-		Solver { items: items, knapsack_size: knapsack_size, memo: memo }
+		let memo_size = items.len() + 1;
+		let mut first_memo = Solver::create_memo(memo_size);
+		let mut second_memo = Solver::create_memo(memo_size);
+		Solver { items: items, knapsack_size: knapsack_size, first_memo: first_memo, second_memo: second_memo }
 	}
 
 	pub fn solve(&mut self) -> u32 {
-		self.reset_memo();
+		self.first_memo.clear();
+		self.second_memo.clear();
+		self.first_memo.push(Memo { optimal_value: 0, used_weight: 0 });
 
-		for item in self.items.iter() {
-			self.step_solution(item);
+		for i in 0..self.items.len() {
+			let (source_memo, target_memo) = match i % 2 {
+				0 => (&self.first_memo, &mut self.second_memo),
+				_ => (&self.second_memo, &mut self.first_memo)
+			};
+			Solver::step_solution(&self.items[i], source_memo, target_memo);
 		}
 
-		self.memo[self.memo.len() - 1].optimal_value
+		self.solution_memo().optimal_value
 	}
 
-	fn step_solution(&mut self, cur_item: &item) {
-		let mut working_weight_memo = Memo { optimal_value: 0, used_weight: 0 };
-		let mut last_weight_memo = Memo { optimal_value: 0, used_weight: 0 };
-
-		for i in 0..self.memo.len() {
-//			let mut ref prior_memo = self.memo[prior_index(i)];
-			let mut ref last_item_memo = self.memo[i];
-			working_weight_memo = optimal_value(&last_weight_memo, last_item_memo, cur_item);
-
-			// Copy over into prior memo
-			last_weight_memo = working_weight_memo;
-			last_item_memo = working_weight_memo;
+	fn solution_memo(&self) -> Memo {
+		if (self.items.len() % 2 == 0) {
+			self.first_memo[self.first_memo.len() - 1]
 		}
+		else {
+			self.second_memo[self.second_memo.len() - 1]
+		}
+	}
+
+	fn step_solution(cur_item: &Item, source_memo: &Vec<Memo>, target_memo: &mut Vec<Memo>) {
+//		let mut working_weight_memo = Memo { optimal_value: 0, used_weight: 0 };
+//		let mut last_weight_memo = Memo { optimal_value: 0, used_weight: 0 };
+//
+//		for i in 0..self.memo.len() {
+//			let mut ref last_item_memo = self.memo[i];
+//			working_weight_memo = optimal_value(&last_weight_memo, last_item_memo, cur_item);
+//
+//			// Copy over into prior memo
+//			last_weight_memo = working_weight_memo;
+//			last_item_memo = working_weight_memo;
+//		}
 	}
 
 //	fn prior_index(&self, cur_index: usize) {
@@ -76,25 +93,15 @@ impl Solver {
 //	}
 
 	fn optimal_value(&self, last_weight_memo: &Memo, last_item_memo: &Memo, item: &Item) -> Memo {
-		composite_memo = last_item_memo.add(item);
-		if composite_memo.makes_weight(knapsack_size) && composite_memo.better_than(last_weight_memo) {
+		let composite_memo = last_item_memo.add(item);
+		if composite_memo.makes_weight(self.knapsack_size) && composite_memo.better_than(last_weight_memo) {
 			return composite_memo;
 		}
-		last_weight_memo
+		*last_weight_memo
 	}
 
-	fn reset_memo(&mut self) {
-		for m in self.memo.iter_mut() {
-			m.optimal_value = 0;
-		}
-	}
-
-	fn create_memo(knapsack_size: u32) -> Vec<Memo> {
-		let mut memo = Vec::<Memo>::with_capacity(knapsack_size as usize);
-		for i in 0..(knapsack_size as usize) {
-			memo.push(Memo { optimal_value: 0} );
-		}
-		memo
+	fn create_memo(memo_size: usize) -> Vec<Memo> {
+		Vec::<Memo>::with_capacity(memo_size)
 	}
 
 	// Greatest common divisor among the weights
