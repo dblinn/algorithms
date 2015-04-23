7f6e764313a3210a4d875eab5381331d3ceb248b
David Blinn <dblinn@cyrusinnovation.com>
2015-04-22 21:45:08 -0400

    update implementation of PathLength enum

--- part2/hw_4/shortest_paths/src/graph.rs
+++ part2/hw_4/shortest_paths/src/graph.rs
@@ -1,4 +1,6 @@
 use std::ops::Add;
+use graph::PathLength::{Reach, Unreach};
+use std::cmp::Ordering;
 
 #[derive(Debug, Clone, Copy)]
 pub struct DirectedEdge {
@@ -45,9 +47,30 @@ impl Node {
 	}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
 pub enum PathLength {
 	Reach(i32),
 	Unreach
+}
+
+impl PathLength {
+	pub fn cat(self, edge: &DirectedEdge) -> PathLength {
+		match self {
+			PathLength::Reach(self_value) => PathLength::Reach(self_value + edge.weight),
+			PathLength::Unreach => PathLength::Unreach,
+		}
+	}
+}
+
+impl Ord for PathLength {
+	fn cmp(&self, other: &Self) -> Ordering {
+		match (*self, *other) {
+			(Reach(self_value), Reach(other_value)) => self_value.cmp(&other_value),
+			(Unreach, Unreach) => Ordering::Equal,
+			(Unreach, _) => Ordering::Greater,
+			(_, Unreach) => Ordering::Less,
+		}
+	}
 }
 
 impl Add for PathLength {
@@ -60,4 +83,32 @@ impl Add for PathLength {
 			(_, PathLength::Unreach) => PathLength::Unreach,
 		}
 	}
+}
+
+#[test]
+fn test_path_add_path() {
+	assert_eq!(Reach(3) + Reach(2), Reach(5));
+	assert_eq!(Reach(3) + Unreach, Unreach);
+	assert_eq!(Unreach + Reach(3), Unreach);
+	assert_eq!(Reach(-3) + Reach(3), Reach(0));
+}
+
+#[test]
+fn test_path_cat_edge() {
+	let de_minus_one = DirectedEdge { weight: -1, a: 0, b: 1 };
+	let de_plus_one = DirectedEdge { weight: 1, a: 0, b: 1 };
+
+	assert_eq!(Reach(3).cat(&de_minus_one), Reach(2));
+	assert_eq!(Reach(3).cat(&de_plus_one), Reach(4));
+	assert_eq!(Unreach.cat(&de_plus_one), Unreach);
+}
+
+#[test]
+fn test_path_comparison() {
+	assert_eq!(Reach(3).cmp(&Reach(4)), Ordering::Less);
+	assert_eq!(Reach(5).cmp(&Reach(4)), Ordering::Greater);
+	assert_eq!(Reach(3).cmp(&Reach(3)), Ordering::Equal);
+	assert_eq!(Reach(3).cmp(&Unreach), Ordering::Less);
+	assert_eq!(Unreach.cmp(&Reach(3)), Ordering::Greater);
+	assert_eq!(Unreach.cmp(&Unreach), Ordering::Equal);
 }
