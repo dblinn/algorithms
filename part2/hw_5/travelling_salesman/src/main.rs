f00e1470037b80354a9d2e483cb062f4cb1c2ccc
David Blinn <dblinn@cyrusinnovation.com>
2015-04-28 23:04:44 -0400

    begin hw_5. create framework with some test examples

---
+++ part2/hw_5/travelling_salesman/src/main.rs
@@ -0,0 +1,34 @@
+mod gosper_subset;
+
+struct Example<'a> {
+	file_name: &'a str,
+	route_distance: f32,
+}
+
+fn main() {
+	let examples = [
+		Example {file_name: "test_cases/rectangle.txt", route_distance: 10f32},
+		Example {file_name: "test_cases/medium.txt", route_distance: 3.50116f32},
+		Example {file_name: "test_cases/tsp.txt", route_distance: std::f32::MAX},
+	];
+
+	for example in examples.iter() {
+		run_example(example);
+	}
+}
+
+fn run_example(example: &Example) {
+//	let (_, edge_count, nodes) = graph_builder::build_graph_from_file(example.file_name);
+}
+
+fn verify_example(example: &Example, found_distance: f32) {
+	if example.route_distance != std::f32::MAX {
+		println!("For example {}, expected route of length {}, found route of length {:?}",
+				 example.file_name, example.route_distance, found_distance);
+		assert_eq!(example.route_distance, found_distance);
+	}
+	else {
+		println!("For example {} found path of length {}",
+				 example.file_name, found_distance);
+	}
+}
