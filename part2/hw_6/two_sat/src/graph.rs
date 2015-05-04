e82d3385c79600375e70ef8364f4fbbe53af263f
David Blinn <dblinn@cyrusinnovation.com>
2015-05-04 18:51:57 -0400

    graph utilities for 2sat

---
+++ part2/hw_6/two_sat/src/graph.rs
@@ -0,0 +1,43 @@
+use std::cmp::Ordering;
+
+#[derive(Debug, Clone, Copy)]
+pub struct DirectedEdge {
+	pub a: usize,
+	pub b: usize,
+}
+
+impl DirectedEdge {
+	pub fn new(a: usize, b: usize) -> DirectedEdge {
+		DirectedEdge { a: a, b: b }
+	}
+}
+
+pub struct NodeBuilder {
+	pub index: usize,
+	pub edge_count: usize,
+}
+
+impl NodeBuilder {
+	pub fn new(index: usize) -> NodeBuilder {
+		NodeBuilder { index: index, edge_count: 0 }
+	}
+
+	pub fn to_node(&self) -> Node {
+		Node::new(self)
+	}
+}
+
+#[derive(Debug)]
+pub struct Node {
+	pub index: usize,
+	pub out_edges: Vec<DirectedEdge>,
+}
+
+impl Node {
+	pub fn new(builder: &NodeBuilder) -> Node {
+		Node {
+			index: builder.index,
+			edges: Vec::<DirectedEdge>::with_capacity(builder.edge_count),
+		}
+	}
+}
