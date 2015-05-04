27314ef2e26c1ba51d1412fc46e6307854dfb837
David Blinn <dblinn@cyrusinnovation.com>
2015-05-04 18:44:04 -0400

    main file for two_sat

--- part2/hw_6/two_sat/src/main.rs
+++ part2/hw_6/two_sat/src/main.rs
@@ -1,3 +1,34 @@
+struct Example<'a> {
+	file_name: &'a str,
+	solveable: Option<bool>,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let examples = [
+		Example {file_name: "test_cases/2sat1.txt", solveable: None},
+		Example {file_name: "test_cases/2sat2.txt", solveable: None},
+		Example {file_name: "test_cases/2sat3.txt", solveable: None},
+		Example {file_name: "test_cases/2sat4.txt", solveable: None},
+		Example {file_name: "test_cases/2sat5.txt", solveable: None},
+		Example {file_name: "test_cases/2sat6.txt", solveable: None},
+	];
+
+	for example in examples.iter() {
+		run_example(example);
+	}
+}
+
+fn run_example(example: &Example) {
+}
+
+fn verify_example(example: &Example, solveable: bool) {
+	if example.solveable != None {
+		println!("For example {}, expected solveability of {}, found solveability of {}",
+				 example.file_name, example.solveable.unwrap(), solveable);
+		assert_eq!(example.solveable.unwrap(), solveable);
+	}
+	else {
+		println!("For example {} found solveability of {}",
+				 example.file_name, solveable);
+	}
 }
